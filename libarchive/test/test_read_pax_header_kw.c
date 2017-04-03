9b6b3648f620a5eb27dfe40bc0afe1c3e4d5e546
Nic Henke <nic.henke@versity.com>
2017-04-03 15:32:13 -0600

    API to add new PAX KW to entry
    There are times where we want to add vendor extension PAX keywords to an
    archive entry. These could be used to add GNU multivolume support or any
    other capability.
    
    There is a new pax option, generic_kw, that can be used to toggle this
    optional behavior.

--- libarchive/test/test_read_pax_header_kw.c
+++ libarchive/test/test_read_pax_header_kw.c
@@ -28,8 +28,12 @@
  */
 
 #include "test.h"
+// /app/bin/libarchive_test -vv -r /app/libarchive/test test_write_pax_kw
 
-DEFINE_TEST(test_pax_kw_shily_xattr)
+static char buff2[64];
+
+
+DEFINE_TEST(test_read_pax_kw)
 {
 	struct archive *a;
 	struct archive_entry *ae;
@@ -73,4 +77,109 @@ DEFINE_TEST(test_pax_kw_shily_xattr)
 	/* Close the archive. */
 	assertEqualInt(ARCHIVE_OK, archive_read_close(a));
 	assertEqualInt(ARCHIVE_OK, archive_read_free(a));
+}
+
+DEFINE_TEST(test_write_pax_kw)
+{
+	size_t buffsize = 1000000;
+	char *buff;
+	struct archive_entry *ae;
+	struct archive *a;
+	size_t used;
+	const char *kw_name = "VENDOR.item";
+	const char *kw_value = "avalue";
+	const char *key;
+	const void *value;
+	size_t value_len;
+
+	buff = malloc(buffsize); /* million bytes of work area */
+	assert(buff != NULL);
+
+	/* Create a new archive in memory. */
+	assert((a = archive_write_new()) != NULL);
+	assertA(0 == archive_write_set_format_pax(a));
+	assertA(0 == archive_write_set_options(a, "pax:generic_kw=1"));
+	assertA(0 == archive_write_add_filter_none(a));
+	assertA(0 == archive_write_open_memory(a, buff, buffsize, &used));
+
+	/*
+	 * "file" has a bunch of attributes and 8 bytes of data.
+	 */
+	assert((ae = archive_entry_new()) != NULL);
+	archive_entry_set_atime(ae, 2, 20);
+	archive_entry_set_birthtime(ae, 3, 30);
+	archive_entry_set_ctime(ae, 4, 40);
+	archive_entry_set_mtime(ae, 5, 50);
+	archive_entry_copy_pathname(ae, "file");
+	archive_entry_set_mode(ae, S_IFREG | 0755);
+	archive_entry_set_size(ae, 8);
+	archive_entry_pax_kw_add_entry(ae, kw_name, kw_value, strlen(kw_value) + 1);
+	assertEqualIntA(a, ARCHIVE_OK, archive_write_header(a, ae));
+	archive_entry_free(ae);
+	assertEqualIntA(a, 8, archive_write_data(a, "12345678", 9));
+
+	/* Close out the archive. */
+	assertEqualIntA(a, ARCHIVE_OK, archive_write_close(a));
+	assertEqualIntA(a, ARCHIVE_OK, archive_write_free(a));
+
+	/*
+	 *
+	 * Now, read the data back.
+	 *
+	 */
+	assert((a = archive_read_new()) != NULL);
+	assertEqualIntA(a, 0, archive_read_support_format_all(a));
+	assertEqualIntA(a, 0, archive_read_support_filter_all(a));
+	assertEqualIntA(a, 0, archive_read_open_memory(a, buff, used));
+
+	/*
+	 * Read "file"
+	 */
+	assertEqualIntA(a, 0, archive_read_next_header(a, &ae));
+	assertEqualInt(2, archive_entry_atime(ae));
+	assertEqualInt(20, archive_entry_atime_nsec(ae));
+	assertEqualInt(3, archive_entry_birthtime(ae));
+	assertEqualInt(30, archive_entry_birthtime_nsec(ae));
+	assertEqualInt(4, archive_entry_ctime(ae));
+	assertEqualInt(40, archive_entry_ctime_nsec(ae));
+	assertEqualInt(5, archive_entry_mtime(ae));
+	assertEqualInt(50, archive_entry_mtime_nsec(ae));
+	assertEqualString("file", archive_entry_pathname(ae));
+	assert((S_IFREG | 0755) == archive_entry_mode(ae));
+	assertEqualInt(8, archive_entry_size(ae));
+
+	assertEqualInt(5, archive_entry_pax_kw_count(ae));
+	assertEqualInt(5, archive_entry_pax_kw_reset(ae));
+
+	/* Validate our attribute of interest -- it got recorded first */
+	assertEqualInt(0, archive_entry_pax_kw_next(ae, &key, &value, &value_len));
+	assertEqualString(key, kw_name);
+	assertEqualString(value, kw_value);
+	/* API is binary, add null termination */
+	assertEqualInt((int)value_len, strlen(value) + 1);
+
+	/* Validate names are still OK, above validated any values we needed to check. */
+	assertEqualInt(0, archive_entry_pax_kw_next(ae, &key, &value, &value_len));
+	assertEqualString(key, "mtime");
+
+	assertEqualInt(0, archive_entry_pax_kw_next(ae, &key, &value, &value_len));
+	assertEqualString(key, "LIBARCHIVE.creationtime");
+
+	assertEqualInt(0, archive_entry_pax_kw_next(ae, &key, &value, &value_len));
+	assertEqualString(key, "atime");
+
+	assertEqualInt(0, archive_entry_pax_kw_next(ae, &key, &value, &value_len));
+	assertEqualString(key, "ctime");
+
+	assertEqualIntA(a, 8, archive_read_data(a, buff2, 10));
+	assertEqualMem(buff2, "12345678", 8);
+
+	/*
+	 * Verify the end of the archive.
+	 */
+	assertEqualIntA(a, ARCHIVE_EOF, archive_read_next_header(a, &ae));
+	assertEqualIntA(a, ARCHIVE_OK, archive_read_close(a));
+	assertEqualIntA(a, ARCHIVE_OK, archive_read_free(a));
+
+	free(buff);
 }
